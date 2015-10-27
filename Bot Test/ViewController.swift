3af501b01e955782f5ce24d3c3eadb48da2f7766
Daniel Tull <dt@danieltull.co.uk>
2015-10-27 09:23:43 +0000

    Initial Commit

---
+++ Bot Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bot Test
+//
+//  Created by Daniel Tull on 27.10.2015.
+//  Copyright © 2015 Daniel Tull. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
