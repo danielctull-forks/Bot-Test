4bc31a2fe14e12eeefa0662e11450bcab558f7be
Daniel Tull <dt@danieltull.co.uk>
2015-10-27 09:30:56 +0000

    Update code

--- Bot Test/ViewController.swift
+++ Bot Test/ViewController.swift
@@ -1,25 +1,5 @@
-//
-//  ViewController.swift
-//  Bot Test
-//
-//  Created by Daniel Tull on 27.10.2015.
-//  Copyright © 2015 Daniel Tull. All rights reserved.
-//
 
 import UIKit
+import DCTAuth
 
-class ViewController: UIViewController {
-
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
-	}
-
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
-	}
-
-
-}
-
+class ViewController: UIViewController {}
